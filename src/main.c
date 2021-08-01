130d1b8f856be32a589d2b35768ce61c5811023c
Ryan <rnleach@users.noreply.github.com>
2021-08-01 09:25:01 -0600

    Set timezone and find max power.

--- src/main.c
+++ src/main.c
@@ -29,9 +29,9 @@ char const *data_dir = "/home/ryan/wxdata/GOES/";
 static void
 program_initialization()
 {
-   setenv("TZ", "UTC", 1);
-   tzset(); 
-   GDALAllRegister();
+    setenv("TZ", "UTC", 1);
+    tzset();
+    GDALAllRegister();
 }
 
 static void
@@ -47,11 +47,26 @@ main()
     DIR *dir = opendir(data_dir);
     Stopif(!dir, return EXIT_FAILURE, "Error opening data directory: %s", data_dir);
 
+    struct Cluster biggest_fire = {0};
+    char biggest_sat[4] = {0};
+    char biggest_sector[5] = {0};
+    time_t biggest_start = 0;
+    time_t biggest_end = 0;
+
     struct dirent *entry = 0;
-    while((entry = readdir(dir))) {
+    while ((entry = readdir(dir))) {
         if (entry->d_type == DT_REG) {
 
-            if(strcmp("nc", file_ext(entry->d_name)) != 0) {
+            if (strcmp("nc", file_ext(entry->d_name)) != 0) {
+                continue;
+            }
+
+            // Skip full disk for now, I don't have many of those and they are much larger.
+            if (strstr(entry->d_name, "FDCF")) {
+                continue;
+            }
+            // Skip meso-sector for now, I don't have many of those.
+            if (strstr(entry->d_name, "FDCM")) {
                 continue;
             }
 
@@ -69,36 +84,44 @@ main()
             remaining = sizeof(full_path) - strnlen(full_path, sizeof(full_path));
             Stopif(remaining <= 0, return EXIT_FAILURE, "path buffer too small");
 
+            printf("Processing: %s\n", entry->d_name);
+            struct ClusterList clusters = cluster_list_from_file(full_path);
+            if (!clusters.error) {
+                for (unsigned int i = 0; i < clusters.clusters->len; ++i) {
+
+                    struct Cluster *curr_clust =
+                        &g_array_index(clusters.clusters, struct Cluster, i);
+
+                    if (curr_clust->power > biggest_fire.power) {
+                        biggest_fire = *curr_clust;
+                        memcpy(biggest_sat, clusters.satellite, 3);
+                        memcpy(biggest_sector, clusters.sector, 4);
+                        biggest_start = clusters.start;
+                        biggest_end = clusters.end;
+                    }
+                }
+            } else {
+                printf("    Error processing file.\n");
+            }
+
+            cluster_list_clear(&clusters);
         }
     }
 
-    struct ClusterList clusters = cluster_list_from_file(fname);
-    g_array_sort(clusters.clusters, cluster_desc_cmp);
-
     char start_str[128] = {0};
-    ctime_r(&clusters.start, start_str);
+    ctime_r(&biggest_start, start_str);
     char end_str[128] = {0};
-    ctime_r(&clusters.end, end_str);
+    ctime_r(&biggest_end, end_str);
 
-    printf("Cluster analysis metadata:\n"
+    printf("\n\nCluster analysis metadata:\n"
            "     satellite: %s\n"
            "        sector: %s\n"
            "         start: %s"
            "           end: %s",
-           clusters.satellite,
-           clusters.sector,
-           start_str,
-           end_str);
+           biggest_sat, biggest_sector, start_str, end_str);
 
-    for (unsigned int i = 0; i < clusters.clusters->len; ++i) {
-
-        struct Cluster *curr_clust = &g_array_index(clusters.clusters, struct Cluster, i);
-
-        printf("Cluster: %2d, Lat: %10.6lf, Lon: %11.6lf, Count: %2d, Power: %5.0lfMW\n", i,
-               curr_clust->lat, curr_clust->lon, curr_clust->count, curr_clust->power);
-    }
-
-    cluster_list_clear(&clusters);
+    printf("Lat: %10.6lf, Lon: %11.6lf, Count: %2d, Power: %5.0lfMW\n", biggest_fire.lat,
+           biggest_fire.lon, biggest_fire.count, biggest_fire.power);
 
     program_finalization();
 }
