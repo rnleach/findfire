2a011771109595189f08373d2167fd9f6c60dc90
Ryan <rnleach@users.noreply.github.com>
2021-09-15 13:15:52 -0600

    Removed some database cruft.

--- src/fire_database/create_fires_db.sql
+++ src/fire_database/create_fires_db.sql
@@ -1,19 +1,3 @@
-CREATE TABLE IF NOT EXISTS clusters
-(
-  rowid       INTEGER PRIMARY KEY AUTOINCREMENT,
-  satellite   TEXT    NOT NULL,
-  sector      TEXT    NOT NULL,
-  start_time  INTEGER NOT NULL,
-  lat         REAL    NOT NULL,
-  lon         REAL    NOT NULL,
-  power       REAL    NOT NULL,
-  cell_count  INTEGER NOT NULL,
-  perimeter   BLOB    NOT NULL
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS no_cluster_dups 
-  ON clusters (satellite, sector, start_time, lat, lon);
-
 CREATE TABLE IF NOT EXISTS fires
 (
     id            TEXT    PRIMARY KEY,
@@ -35,11 +19,5 @@ CREATE TABLE IF NOT EXISTS associations
 (
     cluster_row_id INTEGER,
     fire_id        TEXT
-    -- TODO add foreign key constraint back. Write code to ensure it's
-    --      not violated.
-    -- FOREIGN KEY (cluster_row_id) REFERENCES clusters (rowid),
-    -- FOREIGN KEY (fire_id) REFERENCES fires (id)
 );
-
---PRAGMA foreign_keys = ON;
 
