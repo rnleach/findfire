5dd71e58eea9b3b2e3b6a48685fcdfad401562d9
Ryan <rnleach@users.noreply.github.com>
2021-11-02 09:08:37 -0600

    update documentation.

--- src/util.h
+++ src/util.h
@@ -1,4 +1,9 @@
 #pragma once
+/** \file util.h
+ * \brief Utility functions and macros used throughout the project.
+ *
+ * The things in this module don't fit nicely in other modules.
+ */
 
 #include <assert.h>
 #include <stdio.h>
@@ -9,7 +14,11 @@
 /*-------------------------------------------------------------------------------------------------
  *                                        Error handling.
  *-----------------------------------------------------------------------------------------------*/
-/** Clean error handling. */
+/** \brief Clean error handling not removed in release builds.
+ *
+ * Unlike \c assert, this macro isn't compiled out if \c NDEBUG is defined. Error checking that is
+ * always on.
+ */
 #define Stopif(assertion, error_action, ...)                                                       \
     {                                                                                              \
         if (assertion) {                                                                           \
@@ -24,16 +33,20 @@
 /*-------------------------------------------------------------------------------------------------
  *                                     File name handling.
  *-----------------------------------------------------------------------------------------------*/
-/** Find the file extension.
+/** \brief Find the file extension.
  *
- * Finds the part of the path after the last '.' in the file name (fname).
+ * Finds the part of the path after the last '.' in the file name (\a fname).
  *
- * \returns A pointer into the original fname to the start of the extension. If the file name
+ * \param fname should be a file name without any slashes, '/'. That is, it's just the file name
+ * not the containing directories. If it does include the containing directories, it still may
+ * work, but the error conditions discussed below may not.
+ *
+ * \returns A pointer into the original \a fname to the start of the extension. If the file name
  * doesn't contain a '.' character, then it returns a pointer to an empty string, "".
  */
 char const *file_ext(const char *fname);
 
-/** Find the file name in a path.
+/** \brief Find the file name in a path.
  *
  * \returns a pointer to the first character after the last '/' character. This is not guaranteed
  * to be a file name, it could be a directory if the path didn't include a file name. If there is
@@ -44,7 +57,7 @@ char const *get_file_name(char const *full_path);
 /*-------------------------------------------------------------------------------------------------
  *                                     Time parsing.
  *-----------------------------------------------------------------------------------------------*/
-/** Parse a date-time from a substring from a file name.
+/** \brief Parse a date-time from a substring from a file name.
  *
  * The GOES data stored via the NOAA Big Data initiative is stored in files that include the
  * scan start and end times in the file names. The format of that time stamp is YYYYJJJHHMMSS,
@@ -69,7 +82,7 @@ time_t parse_time_string(char const *tstr);
 #define DIR_STACK_DEPTH 10
 #define MAX_PATH_LEN 256
 
-/** A stack to keep track of the state while we walk a directory tree.
+/** \brief A stack to keep track of the state while we walk a directory tree.
  *
  * Order doesn't matter (inorder, preorder, postorder, level order). This just visits every file
  * in a directory tree starting with the given root.
