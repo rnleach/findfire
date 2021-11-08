ad975534f014ef5851977c07151cedd28b90c743
Ryan <rnleach@users.noreply.github.com>
2021-11-08 12:18:10 -0700

    Update comments

--- src/firepoint.c
+++ src/firepoint.c
@@ -1,4 +1,4 @@
 #include "firepoint.h"
-#include "geo.h"
 
-#include <math.h>
+// Keeping the module intact for the expectation of future additions. Namely, I plan to expand the 
+// FirePoint struct to include more information other than just the power.
