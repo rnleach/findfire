90bafb3d9fd91bab6fbd3d56077b0989dc52e196
Ryan <rnleach@users.noreply.github.com>
2021-11-11 00:08:14 -0700

    Upgrade courier.

--- src/courier.h
+++ src/courier.h
@@ -23,7 +23,7 @@
  */
 
 /*
- * Version 2.0.0 - November 4th, 2021
+ * Version 2.0.1 - November 11th, 2021
  */
 
 /** \file courier.h
@@ -176,15 +176,19 @@ courier_register_sender(Courier *cr)
 {
     assert(cr);
 
-    // Lock not needed because num_producers is atomic
+    int rc = pthread_mutex_lock(&cr->mtx);
+    assert(!rc);
+
     cr->num_producers += 1;
 
     // Broadcast here so any threads blocked in courier_wait_until_ready_to_receive() can progress.
-    int rc = pthread_cond_broadcast(&cr->data_available);
-
+    rc = pthread_cond_broadcast(&cr->data_available);
     if (rc) {
         fputs("Error broadcasting data_available after registering sender!\n", stderr);
     }
+
+    rc = pthread_mutex_unlock(&cr->mtx);
+    assert(!rc);
 }
 
 /** Register a Courier for receiving data. */
@@ -193,15 +197,20 @@ courier_register_receiver(Courier *cr)
 {
     assert(cr);
 
-    // Lock not needed because num_consumers is atomic
+    int rc = pthread_mutex_lock(&cr->mtx);
+    assert(!rc);
+
     cr->num_consumers += 1;
 
     // Broadcast here so any threads blocked in courier_wait_until_ready_to_send() can progress.
-    int rc = pthread_cond_broadcast(&cr->space_available);
+    rc = pthread_cond_broadcast(&cr->space_available);
 
     if (rc) {
         fputs("Error broadcasting space_available after registering receiver!\n", stderr);
     }
+
+    rc = pthread_mutex_unlock(&cr->mtx);
+    assert(!rc);
 }
 
 /** Close a Courier for sending data. */
@@ -211,15 +220,21 @@ courier_done_sending(Courier *cr)
     assert(cr);
     assert(cr->num_producers > 0);
 
+    int rc = pthread_mutex_lock(&cr->mtx);
+    assert(!rc);
+
     cr->num_producers -= 1;
 
     // Broadcast in case anyone is waiting for data to come available that won't!
     // This will let them check the num_producers variable and realize nothing is coming.
-    int rc = pthread_cond_broadcast(&cr->data_available);
+    rc = pthread_cond_broadcast(&cr->data_available);
 
     if (rc) {
         fputs("Error broadcasting data_available after deregistering sender!\n", stderr);
     }
+
+    rc = pthread_mutex_unlock(&cr->mtx);
+    assert(!rc);
 }
 
 /** Close a Courier for receiving data. */
@@ -229,16 +244,22 @@ courier_done_receiving(Courier *cr)
     assert(cr);
     assert(cr->num_consumers > 0);
 
+    int rc = pthread_mutex_lock(&cr->mtx);
+    assert(!rc);
+
     cr->num_consumers -= 1;
 
     // Broadcast in case anyone is waiting to send data that they never will be able too!
     // This will let them check the num_consumers variable and realize space will never become
     // available.
-    int rc = pthread_cond_broadcast(&cr->space_available);
+    rc = pthread_cond_broadcast(&cr->space_available);
 
     if (rc) {
         fputs("Error broadcasting space_available after deregistering receiver!\n", stderr);
     }
+
+    rc = pthread_mutex_unlock(&cr->mtx);
+    assert(!rc);
 }
 
 /** Push a value onto the queue.
