782cad6cd15c2fe18e43dbf77238da96f225c8a6
Ryan <rnleach@users.noreply.github.com>
2021-12-16 09:09:03 -0700

    Progress setting up connectfire program.

--- mains/connectfire.c
+++ mains/connectfire.c
@@ -4,8 +4,8 @@
  * Connect clusters from the output database of findfire to make time series of fires. Each time
  * series is given an ID and stored in a database with a start date and an end date. In the future
  * other statistics may be added to that database. Another table in the database will record the
- * relationship to clusters by associating a row number from the findfire.sqlite database with a
- * fire ID from the database created by this program.
+ * relationship to clusters by associating a row number from the sqlite database with a fire ID 
+ * from the database table created by this program.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,45 +13,94 @@
 
 #include "satfire.h"
 
-/**
- * \brief A wildfire.
- *
- * The id field identifies the relationship between clusters, if there is one. Each fire id will be
- * a 9 digit number followed by an underscore, then a 6 digit sub-fire number. Sub fires are
- * temporally contiguous, meaning they are matched in the next time step. If a time step comes up
- * where a sub fire disappears, then it's done, and if a cluster or hot spot appears there again it
- * will have a new sub-fire number. The fire (determined by the first 9 digits) may go undetected
- * for several days, even a month or more and still persist.
- *
- * e.g. id = 000001239_000056 is sub-fire number 56 of fire number 1239.
- *
- * \todo Still need implmented.
- */
-struct Wildfire {
-    time_t first_observed;
-    time_t last_observed;
-    unsigned int next_subfire;
-    struct SFPixelList *area;
-    char id[16];
+#include "sf_util.h"
+
+/*-------------------------------------------------------------------------------------------------
+ *                          Program Initialization, Finalization, and Options
+ *-----------------------------------------------------------------------------------------------*/
+static struct ConnectFireOptions {
+    char *database_file;
+    bool verbose;
+
+} options = {0};
+
+// clang-format off
+static GOptionEntry option_entries[] = 
+{
+    {
+        "verbose", 
+        'v', 
+        G_OPTION_FLAG_NONE, 
+        G_OPTION_ARG_NONE, 
+        &options.verbose, 
+        "Show verbose output.", 
+        0
+    },
+
+    {NULL}
 };
+// clang-format on
 
 static void
-program_initialization()
+program_initialization(int argc[static 1], char ***argv)
 {
     // Force to use UTC timezone.
     setenv("TZ", "UTC", 1);
     tzset();
+
+    satfire_initialize();
+
+    // Initialize with with environment variables and default values.
+    if (getenv("CLUSTER_DB")) {
+        asprintf(&options.database_file, "%s", getenv("CLUSTER_DB"));
+    }
+
+    // Parse command line options.
+    GError *error = 0;
+    GOptionContext *context = g_option_context_new("- Temporally connect clusters to form fires.");
+    g_option_context_add_main_entries(context, option_entries, 0);
+    g_option_context_parse(context, argc, argv, &error);
+    Stopif(error, exit(EXIT_FAILURE), "Error parsing options: %s", error->message);
+    g_option_context_free(context);
+
+    Stopif(!options.database_file, exit(EXIT_FAILURE), "Invalid, database_file is NULL");
+
+    // Print out options as configured.
+    if (options.verbose) {
+        fprintf(stdout, "  Database: %s\n", options.database_file);
+    }
+
+    satfire_cluster_db_initialize(options.database_file);
 }
 
 static void
 program_finalization()
 {
+    free(options.database_file);
+
+    satfire_finalize();
 }
 
+/*-------------------------------------------------------------------------------------------------
+ *                                             Wildfire
+ *-----------------------------------------------------------------------------------------------*/
+/** \brief A wildfire. */
+struct Wildfire {
+    time_t first_observed;
+    time_t last_observed;
+    struct SFCoord centroid;
+    unsigned int id;
+    struct SFPixelList *area;
+    enum SFSatellite sat;
+};
+
+/*-------------------------------------------------------------------------------------------------
+ *                                             Main
+ *-----------------------------------------------------------------------------------------------*/
 int
-main()
+main(int argc, char *argv[argc + 1])
 {
-    program_initialization();
+    program_initialization(&argc, &argv);
 
     program_finalization();
 
